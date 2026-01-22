a6b760e108ecf2614b39937d9b35e4cbba8918f4
sblinch <sblinch@centova.com>
2026-01-21 16:45:16 -0800

    Add various helper methods to Value

--- value.go
+++ value.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"unicode/utf8"
 )
 
 type Value struct {
@@ -47,7 +48,7 @@ func (v *Value) getResolvedValue() reflect.Value {
 
 // IsString checks whether the underlying value is a string
 func (v *Value) IsString() bool {
-	return v.getResolvedValue().Kind() == reflect.String
+	return v.getResolvedValue().Kind() == reflect.String || v.IsTemplate()
 }
 
 // IsBool checks whether the underlying value is a bool
@@ -76,6 +77,11 @@ func (v *Value) IsInteger() bool {
 		kind == reflect.Uint64
 }
 
+func (v *Value) Is64BitInteger() bool {
+	kind := v.getResolvedValue().Kind()
+	return kind == reflect.Int64 || kind == reflect.Uint64
+}
+
 // IsNumber checks whether the underlying value is either an integer
 // or a float.
 func (v *Value) IsNumber() bool {
@@ -92,6 +98,58 @@ func (v *Value) IsTime() bool {
 func (v *Value) IsNil() bool {
 	// fmt.Printf("%+v\n", v.getResolvedValue().Type().String())
 	return !v.getResolvedValue().IsValid()
+}
+
+func (v *Value) IsScalar() bool {
+	kind := v.getResolvedValue().Kind()
+	switch kind {
+	case reflect.String,
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64,
+		reflect.Bool:
+		return true
+	case reflect.Struct:
+		return v.IsTemplate()
+	}
+	return false
+}
+
+func (v *Value) IsTemplate() bool {
+	rv := v.getResolvedValue()
+	if rv.Kind() == reflect.Struct {
+		_, isTemplate := rv.Interface().(Template)
+		return isTemplate
+	}
+	return false
+}
+
+func (v *Value) EvaluateTemplate(ctx Context) (*Value, error) {
+	val := v.val
+	var (
+		prev reflect.Value
+	)
+	for val.IsValid() {
+		switch val.Kind() {
+		case reflect.Interface, reflect.Ptr:
+			prev = val
+			val = val.Elem()
+		case reflect.Struct:
+			tpl, isTemplate := prev.Interface().(*Template)
+			if !isTemplate {
+				return AsValue(nil), nil
+			}
+			evaluated, err := tpl.Evaluate(ctx)
+			if err != nil {
+				return nil, err
+			}
+			return AsValue(evaluated), nil
+		default:
+			return AsValue(nil), nil
+		}
+	}
+
+	return AsValue(nil), nil
 }
 
 // String returns a string for the underlying value. If this value is not
@@ -154,14 +212,69 @@ func (v *Value) Integer() int {
 	case reflect.Float32, reflect.Float64:
 		return int(rv.Float())
 	case reflect.String:
+		s := rv.String()
+		if len(s) > 1 && s[0] == '0' {
+			switch s[1] {
+			case 'x':
+				i, _ := strconv.ParseInt(s[2:], 16, 64)
+				return int(i)
+			case 'b':
+				i, _ := strconv.ParseInt(s[2:], 2, 64)
+				return int(i)
+			case 'o', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+				i, _ := strconv.ParseInt(s[2:], 8, 64)
+				return int(i)
+			}
+		}
 		// Try to convert from string to int (base 10)
-		f, err := strconv.ParseFloat(rv.String(), 64)
+		f, err := strconv.ParseFloat(s, 64)
 		if err != nil {
 			return 0
 		}
 		return int(f)
 	default:
 		logf("Value.Integer() not available for type: %s\n", rv.Kind().String())
+		return 0
+	}
+}
+
+// Int64 is like Integer but returns an int64
+func (v *Value) Int64() int64 {
+	rv := v.getResolvedValue()
+	switch rv.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return rv.Int()
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		u := rv.Uint()
+		if u > math.MaxInt64 {
+			return math.MaxInt64
+		}
+		return int64(u)
+	case reflect.Float32, reflect.Float64:
+		return int64(rv.Float())
+	case reflect.String:
+		s := rv.String()
+		if len(s) > 1 && s[0] == '0' {
+			switch s[1] {
+			case 'x':
+				i, _ := strconv.ParseInt(s[2:], 16, 64)
+				return i
+			case 'b':
+				i, _ := strconv.ParseInt(s[2:], 2, 64)
+				return i
+			case 'o', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+				i, _ := strconv.ParseInt(s[2:], 8, 64)
+				return i
+			}
+		}
+		// Try to convert from string to int (base 10)
+		f, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			return 0
+		}
+		return int64(f)
+	default:
+		logf("Value.Int64() not available for type: %s\n", rv.Kind().String())
 		return 0
 	}
 }
@@ -335,6 +448,20 @@ func (v *Value) Index(i int) *Value {
 	}
 }
 
+// SetIndex sets the i-th item of an array or slice. Will panic if the underlying value of val does not match the
+// element type of the array or slice.
+func (v *Value) SetIndex(i int, val *Value) {
+	rv := v.getResolvedValue()
+	switch rv.Kind() {
+	case reflect.Array, reflect.Slice:
+		if i < v.Len() {
+			rv.Index(i).Set(val.getResolvedValue())
+		}
+	default:
+		logf("Value.SetIndex() not available for type: %s\n", rv.Kind().String())
+	}
+}
+
 // Contains checks whether the underlying value (which must be of type struct, map,
 // string, array or slice) contains of another Value (e. g. used to check
 // whether a struct contains of a specific field or a map contains a specific key).
@@ -398,6 +525,15 @@ func (v *Value) IsSliceOrArray() bool {
 	return false
 }
 
+// IsIterable checks whether the underlying value is iterable
+func (v *Value) IsIterable() bool {
+	switch v.getResolvedValue().Kind() {
+	case reflect.Array, reflect.Slice, reflect.Map, reflect.String:
+		return true
+	}
+	return false
+}
+
 // IsMap checks whether the underlying value is a map
 func (v *Value) IsMap() bool {
 	return v.getResolvedValue().Kind() == reflect.Map
@@ -406,6 +542,51 @@ func (v *Value) IsMap() bool {
 // IsStruct checks whether the underlying value is a struct
 func (v *Value) IsStruct() bool {
 	return v.getResolvedValue().Kind() == reflect.Struct
+}
+
+// Element returns a Value of the key name from a map[string]T. If the underlying value is not a map, an empty value
+// will be returned.
+func (v *Value) Element(name string) *Value {
+	rv := v.getResolvedValue()
+	for rv.Kind() == reflect.Interface && !rv.IsNil() {
+		rv = rv.Elem()
+	}
+	switch rv.Kind() {
+	case reflect.Map:
+		v := rv.MapIndex(reflect.ValueOf(name))
+		return &Value{val: v}
+	}
+	return &Value{}
+}
+
+func (v *Value) SetElement(name string, value *Value) {
+	rv := v.getResolvedValue()
+	switch rv.Kind() {
+	case reflect.Map:
+		//		println("set element",name,"of",v.String(),"to",value.String())
+		rv.SetMapIndex(reflect.ValueOf(name), value.getResolvedValue())
+	}
+}
+
+// NestedElement is similar to Element, but accepts a string slice of strings representing a list of nested keys. For
+// example, names={"foo","bar"} would attempt to return v.Element("foo").Element("bar").
+func (v *Value) NestedElement(names []string) *Value {
+	r := v
+	for len(names) > 0 {
+		if !r.IsMap() {
+			return &Value{}
+		}
+		r = r.Element(names[0])
+		names = names[1:]
+	}
+
+	return r
+}
+
+// Attribute returns the specified map attribute if the underlying value is a map. Dot-separated values are supported
+// to access nested keys, eg: Attribute("foo") == map["foo"] and Attribute("foo.bar") == map["foo"]["bar"].
+func (v *Value) Attribute(attribute string) *Value {
+	return v.NestedElement(strings.Split(attribute, "."))
 }
 
 // GetItem retrieves a value from a map by key or a field from a struct by name.
@@ -561,6 +742,144 @@ func (v *Value) IterateOrder(fn func(idx, count int, key, value *Value) bool, em
 	empty()
 }
 
+// MapFunc is the function called by Map() for each item in the Iterable. Note that unlike Iterate which (for backward
+// compatibility) provides the item value as `key` when iterating over slices, Map always provides the item value as
+// `value` for maps, slices, and strings. For slices and strings, `key` is nil.
+type MapFunc func(idx, count int, key, value *Value) (newKey, newValue *Value, err error)
+
+// Map is similar to Iterate, but it returns a new Value of the same kind as the underlying value which contains the
+// Values returned by fn. If fn returns (nil, nil, nil) for any item, it will be omitted. If fn returns a non-nil Error,
+// Map will abort and return the same Error with a nil Value.
+//
+// The types returned are:
+// - If the underlying value is a Map: map[string]interface{}
+// - If a slice: []interface{}
+// - If a string: string if all returned Values are strings, bytes, or runes; otherwise []interface{}
+// - Otherwise: nil
+func (v *Value) Map(fn MapFunc) (*Value, error) {
+	switch v.getResolvedValue().Kind() {
+	case reflect.Map:
+		keys := sortedKeys(v.getResolvedValue().MapKeys())
+		keyLen := len(keys)
+		r := make(map[string]interface{}, keyLen)
+		for idx, key := range keys {
+			value := v.getResolvedValue().MapIndex(key)
+			newKey, newValue, err := fn(idx, keyLen, &Value{val: key}, &Value{val: value})
+			if err != nil {
+				return nil, err
+			}
+			if newKey != nil && newValue != nil {
+				r[newKey.String()] = newValue.Interface()
+			}
+		}
+		return AsValue(r), nil
+	case reflect.Array, reflect.Slice:
+		itemCount := v.getResolvedValue().Len()
+		r := make([]interface{}, 0, itemCount)
+		for idx := 0; idx < itemCount; idx++ {
+			_, newItem, err := fn(idx, itemCount, nil, &Value{val: v.getResolvedValue().Index(idx)})
+			if err != nil {
+				return nil, err
+			}
+			if newItem != nil {
+				r = append(r, newItem.Interface())
+			}
+		}
+		return AsValue(r), nil
+	case reflect.String:
+		inputString := v.getResolvedValue().String()
+		charCount := utf8.RuneCountInString(inputString)
+
+		var (
+			b strings.Builder
+			r []interface{}
+		)
+		toString := true
+
+	stringAgain:
+		for i, c := range inputString {
+			newC, _, err := fn(i, charCount, nil, &Value{val: reflect.ValueOf(c)})
+			if err != nil {
+				return nil, err
+			}
+			if newC == nil {
+				continue
+			}
+
+			if toString {
+				switch newC.getResolvedValue().Kind() {
+				case reflect.Int8, reflect.Int32:
+					// we can't know for certain that the map function is returning byte/rune values in this case, but it's
+					// probably a fair bet
+					b.WriteRune(rune(newC.Integer()))
+				case reflect.String:
+					b.WriteString(newC.String())
+				default:
+					toString = false
+					r = make([]interface{}, 0, charCount)
+					goto stringAgain
+				}
+			} else {
+				r = append(r, newC.Interface())
+			}
+		}
+
+		if toString {
+			return AsValue(b.String()), nil
+		} else {
+			return AsValue(r), nil
+		}
+	default:
+		//		return nil, &Error{OrigError: fmt.Errorf("cannot apply map function to value of type %s", v.getResolvedValue().Kind().String())}
+		return AsValue(nil), nil
+	}
+}
+
+// ShallowCopy makes a shallow copy of the underlying value. If the underlying value is wrapped in pointer or
+// interface{} values, those layers of indirection are removed in the returned value; otherwise the returned value is of
+// the same type as the original.
+func (v *Value) ShallowCopy() *Value {
+	var r reflect.Value
+	rv := v.getResolvedValue()
+	wasPtr := v.val.Kind() == reflect.Ptr
+
+	if v.IsMap() {
+		r = reflect.MakeMapWithSize(rv.Type(), v.Len())
+		for _, k := range rv.MapKeys() {
+			r.SetMapIndex(k, rv.MapIndex(k))
+		}
+
+	} else if v.IsSliceOrArray() {
+		r = reflect.MakeSlice(rv.Type(), 0, v.Len())
+		for i := 0; i < v.Len(); i++ {
+			r = reflect.Append(r, rv.Index(i))
+		}
+
+	} else if rv.Kind() == reflect.Struct {
+		r = reflect.New(rv.Type())
+		rmap := r
+		if rmap.Kind() == reflect.Ptr {
+			rmap = rmap.Elem()
+		}
+		for i := 0; i < rv.NumField(); i++ {
+			fv := rv.Field(i)
+			rmap.Field(i).Set(fv)
+		}
+
+		if !wasPtr {
+			r = rmap
+		}
+
+	} else {
+		r = reflect.New(rv.Type())
+		rd := r.Elem()
+		rd.Set(rv)
+		r = rd
+	}
+
+	return &Value{val: r, safe: v.safe}
+}
+
 // Interface gives you access to the underlying value.
 func (v *Value) Interface() any {
 	if v.val.IsValid() {
@@ -601,6 +920,120 @@ func (v *Value) EqualValueTo(other *Value) bool {
 		v.Interface() == other.Interface()
 }
 
+// Less implements sort.Interface, and (when the underlying value is a slice) indicates whether the value at index i
+// is less than the value at index j.
+func (v *Value) Less(i, j int) bool {
+	if !v.IsSliceOrArray() || i >= v.Len() || j >= v.Len() {
+		return false
+	}
+
+	return v.Index(i).Compare(v.Index(j)) == -1
+}
+
+// Swap implements sort.Interface, and (when the underlying value is a slice) swaps the elements with indexes i and j.
+func (v *Value) Swap(i, j int) {
+	if !v.IsSliceOrArray() || i >= v.Len() || j >= v.Len() {
+		return
+	}
+
+	vi := v.Index(i)
+	vj := v.Index(j)
+	v.SetIndex(i, vj)
+	v.SetIndex(j, vi)
+}
+
+func (v *Value) CompareCaseFold(other *Value) int {
+	return v.compare(other, false)
+}
+
+func (v *Value) Compare(other *Value) int {
+	return v.compare(other, true)
+}
+
+func (v *Value) compare(other *Value, caseSensitive bool) int {
+	if !v.val.IsValid() || !v.val.Type().Comparable() {
+		return -1
+	}
+	if !other.val.IsValid() || !other.val.Type().Comparable() {
+		return 1
+	}
+
+	switch {
+	case v.IsInteger() && other.IsInteger():
+		va := v.Int64()
+		vb := other.Int64()
+		if va < vb {
+			return -1
+		} else if va > vb {
+			return 1
+		} else {
+			return 0
+		}
+	case v.IsFloat() && other.IsFloat():
+		va := v.Float()
+		vb := other.Float()
+		if va < vb {
+			return -1
+		} else if va > vb {
+			return 1
+		} else {
+			return 0
+		}
+	case v.IsTime() && other.IsTime():
+		va := v.Time()
+		vb := other.Time()
+		if va.Before(vb) {
+			return -1
+		} else if va.After(vb) {
+			return 1
+		} else {
+			return 0
+		}
+	case v.IsBool() && other.IsBool():
+		va := v.Bool()
+		vb := other.Bool()
+		if va && !vb {
+			return 1
+		} else if vb && !va {
+			return -1
+		} else {
+			return 0
+		}
+	case v.IsSliceOrArray() && other.IsSliceOrArray(), v.IsMap() && other.IsMap():
+		va := v.getResolvedValue().Len()
+		vb := other.getResolvedValue().Len()
+		if va < vb {
+			return -1
+		} else if va > vb {
+			return 1
+		} else {
+			return 0
+		}
+	default:
+		if v.IsNil() && !other.IsNil() {
+			return -1
+		} else if other.IsNil() && !v.IsNil() {
+			return 1
+		}
+
+		va := v.String()
+		vb := other.String()
+
+		if !caseSensitive {
+			va = strings.ToLower(va)
+			vb = strings.ToLower(vb)
+		}
+
+		if va < vb {
+			return -1
+		} else if va > vb {
+			return 1
+		} else {
+			return 0
+		}
+	}
+}
+
 type sortedKeys []reflect.Value
 
 func (sk sortedKeys) Len() int {
@@ -615,8 +1048,10 @@ func (sk sortedKeys) Less(i, j int) bool {
 		return vi.Integer() < vj.Integer()
 	case vi.IsFloat() && vj.IsFloat():
 		return vi.Float() < vj.Float()
-	default:
+	case vi.IsString():
 		return vi.String() < vj.String()
+	default:
+		return vi.Compare(vj) == -1
 	}
 }
 
@@ -638,8 +1073,10 @@ func (vl valuesList) Less(i, j int) bool {
 		return vi.Integer() < vj.Integer()
 	case vi.IsFloat() && vj.IsFloat():
 		return vi.Float() < vj.Float()
-	default:
+	case vi.IsString():
 		return vi.String() < vj.String()
+	default:
+		return vi.Compare(vj) == -1
 	}
 }
 
